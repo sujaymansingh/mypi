1c37ee0d9fe228710f263e9c89fac4ecfffc8131
Sujay Mansingh <sujaymansingh@gmail.com>
2014-05-31 16:24:38 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ REQUIREMENTS = [
 if __name__ == "__main__":
     setuptools.setup(
         name="mypi",
-        version="0.0.2",
+        version="0.0.3",
         author="Sujay Mansingh",
         author_email="sujay.mansingh@gmail.com",
         packages=setuptools.find_packages(),
